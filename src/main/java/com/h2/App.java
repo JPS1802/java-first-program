0ea122d3ef67937abac11fd938cabe479e890aca
JPS1802 <jeanphilippe.schellenberger@capgemini.com>
2020-10-29 18:49:12 +0100

    Update App.java

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -18,5 +18,7 @@ public class App
     private static int add (int[] numbers) {
         int sum = 0;
         for (int i = 0; i < numbers.length ; i++ ) sum = sum + numbers[i];
+        return sum;
+        
     }
 }
