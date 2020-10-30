b8347f0a8a2f7c44b2255e03b8d9a76d06bb1229
JPS1802 <jeanphilippe.schellenberger@capgemini.com>
2020-10-30 17:45:31 +0100

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 import java.util.Map;
 
 public class BestLoanRates {
+    public static float getRates(int loanTermInYears) {return 0.0f;}
+
     public static final Map<Integer, Float> bestRates =
             Map.of(
             1, 5.50f,
