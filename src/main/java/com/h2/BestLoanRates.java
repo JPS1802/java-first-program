19a5b12e06d0ba75d021f19cced129067223f5f3
JPS1802 <jeanphilippe.schellenberger@capgemini.com>
2020-10-30 17:31:40 +0100

    Update BestLoanRates.java

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -1,13 +1,19 @@
 package com.h2;
 
 import java.util.Scanner;
+import java.util.Map;
 
 public class BestLoanRates {
+    public static final Map<Integer, Float> bestRates =
+            Map.of(
+            k1:1, v1:5.50f,
+            k2:2, v2:3.45f,
+            k3:3, v3:2.67f);
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = scanner.nextLine();
         System.out.println("Hello " + name);
     }
-
 }
