e6d00daa6627afb6c71f45e61bdebe3d37415a5d
JPS1802 <jeanphilippe.schellenberger@capgemini.com>
2020-11-03 14:51:41 +0100

    Update Finance.java

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,6 +1,7 @@
 import java.util.Map;
 
 public class Finance {
+
     public final static String BEST_LOAN_RATES = "bestLoanRates";
     public final static String SAVINGS_CALCULATOR = "savingsCalculator";
     public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
@@ -10,4 +11,8 @@ public class Finance {
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>"
     );
+
+    private static boolean validateCommandArguments(String[] args){
+        return false;
+    }
 }
